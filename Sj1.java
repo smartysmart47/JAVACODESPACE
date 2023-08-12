47c8501d478e3f6c28dfc7ff49084d45eef84502
smartysmart47 <139089002+smartysmart47@users.noreply.github.com>
2023-08-12 03:32:55 +0000

    Added a new README

---
+++ Sj1.java
@@ -0,0 +1,27 @@
+public class Sj1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a new java file");
+        byte age = 10;
+        short s = 87;
+        int i = 455;
+        long l = 1441487L;
+        char c = 's';
+        float f =  50.5f;
+        double d = 4.66;
+        boolean b = false;
+        String name = "Shanker";
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(s);
+
+    }
+}
