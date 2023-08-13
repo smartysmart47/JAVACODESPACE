b6e2011a11379a40d58315339b9707e8693adb8e
smartysmart47 <139089002+smartysmart47@users.noreply.github.com>
2023-08-13 12:59:40 +0000

    Pracice set 1

---
+++ chp1Practice.java
@@ -0,0 +1,23 @@
+public class chp1Practice {
+    public static void main(String[] args) {
+        System.out.println("This is a practice set");
+        // question 1
+        int a,b,c;
+        a = 10;
+        b = 20;
+        c = 14;
+        int d = a+b+c;
+        
+        // System.out.println("The sum of a b and c is : " + d);
+        
+        float m1,m2,m3,m4,m5;
+        m1 = 7.5f;
+        m2 = 8.5f;
+        m3 = 9.0f;
+        m4 = 4.9f;
+        m5 = 5.5f;
+        
+        float cgpa = (m1+m2+m3+m4+m5)/4.0f;
+        System.out.println("The cgpa got by student is : " + cgpa);
+    }
+}
